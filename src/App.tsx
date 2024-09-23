29d519128f013b384e714ae118e6995075197ddb
Nuno Alves <nyansword@gmail.com>
2024-09-23 16:23:51 +0000

    Add initial header; Add eslint configs; Add prettier configs; Add i18n; Add tailwind configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import React from 'react';
+import Header from "./components/header";
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="bg-satin-linen-200 h-20 flex flex-row items-center">
+      <h1 className="text-4xl text-center text-satin-linen-800 ml-6">
+        Nuno Alves
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
