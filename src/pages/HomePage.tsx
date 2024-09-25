6b6780e7c21fcde6ae34f39bf922a1e2c24bc717
Nuno Alves <nyansword@gmail.com>
2024-09-25 09:37:57 +0100

    Add gradient

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 function HomePage() {
-  return <div className="h-screen max-h-screen"></div>;
+  return <div className="h-screen max-h-screen bg-gradient-to-b from-satin-linen-200 from-10%"></div>;
 }
 
 export default HomePage;
