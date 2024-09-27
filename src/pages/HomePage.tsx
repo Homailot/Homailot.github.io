c3493429e972fb5df800776ae5284d4c5af6ee0b
Homailot <nyansword@gmail.com>
2024-09-27 19:47:17 +0100

    Add gradient to about text container

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,22 +3,22 @@ import { Trans, useTranslation } from "react-i18next";
 function HomePage() {
   const { t } = useTranslation();
 
-  return <div className="flex flex-row items-center justify-content-center h-screen min-h-screen bg-satin-linen-50">
-    <div className="w-1/2 h-auto bg-satin-linen-100">
-      <div className="flex flex-col justify-center items-start  w-[38rem] p-12">
+  return <div className="flex flex-row items-center justify-center py-16 min-h-screen bg-satin-linen-50">
+    <div className="w-[45rem] h-auto bg-gradient-horizontal-edges">
+      <article className="flex flex-col justify-center items-start m-auto w-[38.5rem] p-12">
         <h1 className="text-4xl font-display font-semibold text-satin-linen-800 mb-2">
           {t("about.greeting")}
         </h1>
-        <p className="text-xl font-body text-satin-linen-800 mb-8 leading-relaxed">
+        <p className="text-xl font-body text-satin-linen-800 mb-8 leading-9">
           <Trans i18nKey="about.part_1" t={t} />
         </p>
-        <p className="text-xl font-body text-satin-linen-800 mb-8 leading-relaxed">
+        <p className="text-xl font-body text-satin-linen-800 mb-8 leading-9">
           {t("about.part_2")}
         </p>
-        <p className="text-xl font-body text-satin-linen-800 leading-relaxed">
+        <p className="text-xl font-body text-satin-linen-800 leading-9">
           {t("about.part_3")}
         </p>
-      </div>
+      </article>
     </div>
   </div>;
 }
