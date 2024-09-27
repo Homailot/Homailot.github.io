f1948e515a2d54657472ff5358d3ff2392c57d04
Homailot <nyansword@gmail.com>
2024-09-27 01:25:48 +0100

    Add about text style

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,19 +4,21 @@ function HomePage() {
   const { t } = useTranslation();
 
   return <div className="flex flex-row items-center justify-content-center h-screen min-h-screen bg-satin-linen-50">
-    <div className="flex flex-col justify-center items-start w-1/2 bg-satin-linen-100">
-      <h1 className="text-7xl font-display font-semibold text-satin-linen-800 ml-8">
-        {t("about.greeting")}
-      </h1>
-      <p className="text-2xl font-body text-satin-linen-800 ml-8">
-        <Trans i18nKey="about.part_1" t={t} />
-      </p>
-      <p className="text-2xl font-body text-satin-linen-800 ml-8">
-        {t("about.part_2")}
-      </p>
-      <p className="text-2xl font-body text-satin-linen-800 ml-8">
-        {t("about.part_3")}
-      </p>
+    <div className="w-1/2 h-auto bg-satin-linen-100">
+      <div className="flex flex-col justify-center items-start  w-[38rem] p-12">
+        <h1 className="text-4xl font-display font-semibold text-satin-linen-800 mb-2">
+          {t("about.greeting")}
+        </h1>
+        <p className="text-xl font-body text-satin-linen-800 mb-8 leading-relaxed">
+          <Trans i18nKey="about.part_1" t={t} />
+        </p>
+        <p className="text-xl font-body text-satin-linen-800 mb-8 leading-relaxed">
+          {t("about.part_2")}
+        </p>
+        <p className="text-xl font-body text-satin-linen-800 leading-relaxed">
+          {t("about.part_3")}
+        </p>
+      </div>
     </div>
   </div>;
 }
